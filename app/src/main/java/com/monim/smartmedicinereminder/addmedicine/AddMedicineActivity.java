6b72251f761be7389312d68891ebad11eef8b97b
Muntasir Hossen <montachirhossen76@gmail.com>
2023-01-14 00:45:30 +0600

    First commit

---
+++ app/src/main/java/com/monim/smartmedicinereminder/addmedicine/AddMedicineActivity.java
@@ -0,0 +1,18 @@
+package com.monim.smartmedicinereminder.addmedicine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.smartmedicinereminder.R;
+
+public class AddMedicineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_medicine);
+
+
+    }
+}
---
+++ app/src/main/java/com/monim/smartmedicinereminder/medicine/MedicineActivity.java
@@ -0,0 +1,16 @@
+package com.monim.smartmedicinereminder.medicine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.smartmedicinereminder.R;
+
+public class MedicineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medicine);
+    }
+}
